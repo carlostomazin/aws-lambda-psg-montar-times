c0a857e44b39d8413030294b343f7ca0b76035a6
Carlos Tomazin <carlos.tomazin2@gmail.com>
2025-11-15 14:31:55 -0300

    init

--- app/src/services/__init__.py
+++ app/src/services/__init__.py
@@ -1,4 +1,4 @@
-from .post_montar_times import post_montar_times
 from .get_recupera_jogos import get_recupera_jogos
+from .post_montar_times import post_montar_times
 
 all = ["post_montar_times", "get_recupera_jogos"]
