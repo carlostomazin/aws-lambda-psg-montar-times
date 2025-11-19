c73e12eccc0621725b98afeda06b799162f4a263
Carlos Tomazin <carlos.tomazin2@gmail.com>
2025-11-19 01:02:55 -0300

    get games

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,19 +1,28 @@
 from aws_lambda_powertools import Logger, Tracer
-from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.event_handler import APIGatewayHttpResolver, Response
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from src.services import get_recupera_jogos, post_gerar_jogo
 
 tracer = Tracer()
 logger = Logger()
 app = APIGatewayHttpResolver()
 
 
-@app.get("/")
 @app.get("/health")
 @tracer.capture_method
 def get_health():
     logger.info("Health check endpoint called")
     return {"status": "ok"}
+
+
+@app.get("/games")
+@tracer.capture_method
+def get_games():
+    status_code, response = get_recupera_jogos()
+    return Response(
+        status_code=status_code, content_type="application/json", body=response
+    )
 
 
 # You can continue to use other utilities just as before
